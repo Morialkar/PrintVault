fc428ced9b39d09fdd4bb64a13dbadca559cfcec
Naomi Gilbert <naomi@morialkar.com>
2024-02-25 04:47:04 +0000

    Let's just make a full next app fullstop

--- apps/api/_context.ts
+++
@@ -1,9 +0,0 @@
-import { GetContext, InitialContext } from 'fuse'
-
-export const getContext = (
-  ctx: InitialContext,
-): GetContext<{ ua: string | null }> => {
-  return {
-    ua: ctx.request.headers.get('user-agent'),
-  }
-}
